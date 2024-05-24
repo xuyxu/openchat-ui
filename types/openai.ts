96ee2c4edfba4507eee9725c6410af93d533a1d5
One <imone@tuta.io>
2024-05-25 01:44:25 +0800

    support for llama3 based models

--- types/openai.ts
+++ types/openai.ts
@@ -1,5 +1,5 @@
 import mistralTokenizer from 'mistral-tokenizer-js';
-import llamaTokenizer from 'llama-tokenizer-js';
+import llama3Tokenizer from 'llama3-tokenizer-js';
 import { OPENAI_API_TYPE } from '../utils/app/const';
 
 export interface OpenAIModel {
@@ -11,8 +11,8 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   // OpenChat
-  OPENCHAT3_2 = 'openchat_v3.2',
   OPENCHAT_3_2_MISTRAL = 'openchat_v3.2_mistral',
+  OPENCHAT_3_6 = 'openchat_3.6'
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
@@ -20,23 +20,23 @@ export const fallbackModelID = OpenAIModelID.OPENCHAT_3_2_MISTRAL;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   // OpenChat
-  [OpenAIModelID.OPENCHAT3_2]: {
-    id: OpenAIModelID.OPENCHAT3_2,
-    name: 'OpenChat 3.2',
-    maxLength: 4096 * 3,
-    tokenLimit: 4096,
-  },
-
   [OpenAIModelID.OPENCHAT_3_2_MISTRAL]: {
     id: OpenAIModelID.OPENCHAT_3_2_MISTRAL,
-    name: 'OpenChat Aura',
+    name: 'OpenChat 3.5 (latest)',
     maxLength: 8192 * 3,
     tokenLimit: 8192,
   },
+
+  [OpenAIModelID.OPENCHAT_3_6]: {
+    id: OpenAIModelID.OPENCHAT_3_6,
+    name: 'OpenChat 3.6 (latest)',
+    maxLength: 8192 * 3,
+    tokenLimit: 8192,
+  }
 };
 
 export const OpenAITokenizers: Record<OpenAIModelID, any> = {
-  [OpenAIModelID.OPENCHAT3_2]: llamaTokenizer,
-
   [OpenAIModelID.OPENCHAT_3_2_MISTRAL]: mistralTokenizer,
+
+  [OpenAIModelID.OPENCHAT_3_6]: llama3Tokenizer, 
 };
